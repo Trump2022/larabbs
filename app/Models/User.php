d6463a8324398d86f22092b96add938d23d85683
Trump2022 <qsw2012@outlook.com>
2022-04-12 19:29:47 +0800

    用户可以删除自己发的话题

--- app/Models/User.php
+++ app/Models/User.php
@@ -78,4 +78,9 @@ class User extends Authenticatable implements MustVerifyEmail
     {
         return $this->hasMany(Topic::class);
     }
+
+    public function isAuthorOf($model)
+    {
+        return $this->id == $model->user_id;
+    }
 }
